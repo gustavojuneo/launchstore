6c171a9b5e62b296edbd702427568fbb63e49bc0
Gustavo Santos <gustavojuneo@outlook.com>
2021-03-05 13:21:15 -0300

    Showing Product page with images and added Lightbox

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -54,7 +54,16 @@ module.exports = {
     product.oldPrice = formatPrice(product.old_price)
     product.price = formatPrice(product.price)
 
-    return res.render('products/show.njk', { product })
+    results = await Product.files(product.id)
+    const files = results.rows.map(file => ({
+      ...file,
+      src: `${req.protocol}://${req.headers.host}${file.path.replace(
+        'public',
+        ''
+      )}`
+    }))
+
+    return res.render('products/show.njk', { product, files })
   },
 
   async edit(req, res) {
